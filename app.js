d70ec968f00a50ca9f62346da2874d6861123d3e
Your Name In Single Quotes <your email here in single quotes>
2019-11-17 00:01:51 +0200

    num

--- app.js
+++ app.js
@@ -110,10 +110,17 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
-}
+    var multiply1 = multiply(multArr[0],multArr[1])[0];
+    var multiply2 = multiply (multiply1,multArr[2])[0];
+    var result = [];
+    result.push(multiply2);
+    result.push("The numbers 2,3,4 have a product of 24.");
+    return result;
+
+    }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
