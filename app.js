65a8701a077d5d09fb17ef1a5ec09c2e675c3860
Your Name In Single Quotes <your email here in single quotes>
2019-11-16 23:40:47 +0200

    sum/

--- app.js
+++ app.js
@@ -81,11 +81,19 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
-}
+var sum1 = sum(sumArr[0],sumArr[1])[0];
+var sum2 = sum(sum1,sumArr[2])[0];
+var result = [];
+result.push (sum2);
+result.push ("2,3,4 was passed in as an array of numbers, and 9 is their sum.");
+return result;
+ }
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
